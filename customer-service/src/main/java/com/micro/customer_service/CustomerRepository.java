099e12753d8b25950fabded43d155e72169eef67
HALIMA DAOUDI <daoudi.halima@yahoo.com>
2024-08-09 23:25:33 +0100

    Second commit

---
+++ customer-service/src/main/java/com/micro/customer_service/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.micro.customer_service;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
